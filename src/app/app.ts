d2bb070c88e7c1ae27ed896705be6fa20c9ce3ef
givanyo <giovanniviana22@gmail.com>
2026-09-23 22:21:48 -0300

    feat: secao sobre

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
+import { About } from './about/about';
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, About],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
